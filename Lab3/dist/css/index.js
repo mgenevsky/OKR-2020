233965661f06500783f50eb9adb284565886aa3d
Geferli <maksgenevsky@gmail.com>
2020-11-02 09:41:04 +0200

    Lab3

--- Lab3/dist/css/index.js
+++ Lab3/dist/css/index.js
@@ -4,48 +4,69 @@ if (performance.navigation.type == 1) {
     if(window.confirm(`На этой странице были созданы куки: \n${document.cookie} \n Хотите ли Вы их сохранить?`)) 
     alert('Куки были сохранены, страница будет обновлена...');
     else{
-    	setCookie('maxNumber', '');
-    	setCookie('minNumber', '');
+      setCookie('maxNumber', '');
+      setCookie('minNumber', '');
     } 
   }
 }
-swap('#name1', '#name2');
-countS(3,4, '#section-4');
-if(checkCookie('maxNumber')) document.querySelector('#numForm').remove();
-makeEditableBlock('section-2');
+swap('#name1', '#name2'); //1
+countS(3,4, '#section-4'); //2
+if(checkCookie('maxNumber')) document.querySelector('#numForm').remove(); //3
+MaxMin('#numBtn','#input1','#input2','#input3','#input4','#input5','#input6','#input7','#input8','#input9','#input10');
+if( window.localStorage ){ //4
+  if(localStorage.getItem('check')==null){
+    localStorage.setItem('check',0);
+    document.querySelector('#midbold').checked = true;
+  }
+  else if(localStorage.getItem('check')==0){
+    document.querySelector('#section-5').style.fontWeight = 'normal';
+    document.querySelector('#midbold').checked = true;
+  }
+  else {
+    document.querySelector('#section-5').style.fontWeight = 'bold';
+    document.querySelector('#bold').checked = true;
+  }
+}
+else alert(" localStorage cant be used");
+
+
+
+makeEditableBlock('section-2');//6
 makeEditableBlock('section-5');
 initEditableBlocks();
 })
 
 const swap = (id1, id2) => { //task 1
-	let tmp = document.querySelector(id1).innerHTML;
-	document.querySelector(id1).innerHTML = document.querySelector(id2).innerHTML;
-	document.querySelector(id2).innerHTML = tmp;
+  let tmp = document.querySelector(id1).innerHTML;
+  document.querySelector(id1).innerHTML = document.querySelector(id2).innerHTML;
+  document.querySelector(id2).innerHTML = tmp;
 }
 
 
 const countS = (a = 0, b=0 ,outputId) => { //task 2
-	let thirdSect = document.querySelector(outputId);
-	let s = a*b;
-	thirdSect.append('S = ' + s);
+  let thirdSect = document.querySelector(outputId);
+  let s = a*b;
+  thirdSect.append('S = ' + s);
 }
 
-
-document.querySelector('#numBtn').addEventListener('click', () => { //task 3
-    let mass = [document.querySelector('#input1').value,
-                document.querySelector('#input2').value,
-                document.querySelector('#input3').value,
-                document.querySelector('#input4').value,
-                document.querySelector('#input5').value,
-                document.querySelector('#input6').value,
-                document.querySelector('#input7').value,
-                document.querySelector('#input8').value,
-                document.querySelector('#input9').value,
-                document.querySelector('#input10').value];
+const MaxMin = (name,i1,i2,i3,i4,i5,i6,i7,i8,i9,i10) => {
+  document.querySelector(name).addEventListener('click', () => { //task 3
+    let mass = [document.querySelector(i1).value,
+                document.querySelector(i2).value,
+                document.querySelector(i3).value,
+                document.querySelector(i4).value,
+                document.querySelector(i5).value,
+                document.querySelector(i6).value,
+                document.querySelector(i7).value,
+                document.querySelector(i8).value,
+                document.querySelector(i9).value,
+                document.querySelector(i10).value];
     alert('Самое большое число: ' + Math.max(...mass)+' \nСамое маленькое число: ' + Math.min(...mass));
     setCookie('maxNumber', Math.max(...mass), 2);
     setCookie('minNumber', Math.min(...mass), 3);
 })
+}
+
 const setCookie = (name, data, expDays) => {
  const d = new Date();
  d.setDate(d.getDate() + expDays);
@@ -59,29 +80,27 @@ const getCookie = (name) => {
 }
 
 
-if( window.localStorage ){ //task 4
- if(localStorage.getItem('check')==null){localStorage.setItem('check',0);}
-else if(localStorage.getItem('check')==0){document.querySelector('#section-5').style.fontWeight = 'normal'}
-	else {document.querySelector('#section-5').style.fontWeight = 'bold'}
+
+function clickMidBold() { //task 4
+    if (document.querySelector('#midbold').checked === true){
+      document.querySelector('#section-5').style.fontWeight = 'normal';
+      localStorage.setItem('check',0);
+    }
+    if (document.querySelector('#midbold').checked === false){
+      document.querySelector('#section-5').style.fontWeight = 'bold';
+      localStorage.setItem('check',1);
+    }
 }
-else alert(" localStorage cant be used");
-function clickMeBold() {
-    if (document.querySelector('#medbold').checked === true){
-        document.querySelector('#section-5').style.fontWeight = 'normal';
-    }
-    if (document.querySelector('#medbold').checked === false){
-        document.querySelector('#section-5').style.fontWeight = 'bold';
-    }
-        if (document.querySelector('#bold').checked === false){
-        localStorage.setItem('check',0);
-        document.querySelector('#section-5').style.fontWeight = 'normal';
+function clickBold() {
+    if (document.querySelector('#bold').checked === false){
+      localStorage.setItem('check',0);
+      document.querySelector('#section-5').style.fontWeight = 'normal';
     }
     if (document.querySelector('#bold').checked === true){
-    	localStorage.setItem('check',1);
-        document.querySelector('#section-5').style.fontWeight = 'bold';
+      localStorage.setItem('check',1);
+      document.querySelector('#section-5').style.fontWeight = 'bold';
     }
 }
-
 
 input.onblur = function() { //task 5
   if (!input.value.includes('@')) {
@@ -119,13 +138,13 @@ const initEditableBlocks = () => { //task 6
   })
 }
 const makeEditableBlock = (blockId) => {
-	const content = localStorage.getItem(`${blockId}Content`) ? 
-	localStorage.getItem(`${blockId}Content`) : 
-	document.getElementById(blockId).innerHTML;
+  const content = localStorage.getItem(`${blockId}Content`) ? 
+  localStorage.getItem(`${blockId}Content`) : 
+  document.getElementById(blockId).innerHTML;
   document.getElementById(blockId).innerHTML = content;
   document.getElementById(blockId).insertAdjacentHTML('beforeend', 
   `<textarea class="editArea">${content}</textarea>
-  <button type="submit" class="editBtn">Return default</button>`)
+  <button type="submit" class="editBtn">Return</button>`)
 }
 const isValidHTML = (html) => {
  const doc = document.createElement('div');
